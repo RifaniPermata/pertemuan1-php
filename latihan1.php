dc3788b1de5d67f3533426fd2c2348826960a585
RifaniPermata <53283788+RifaniPermata@users.noreply.github.com>
2019-10-04 09:34:08 +0700

    php1

---
+++ latihan1.php
@@ -0,0 +1,20 @@
+<?php
+	//echo "HELLO WORD!";
+	//echo "<h1>HALO DUNIA</h1>";
+
+	//variabel
+	//diawali dengan tanda$
+	//nama variabletidak boleh diawali dengan angka
+	//boleh mengandung angka
+			//$nama = "Rifani Permata";
+	        //$pesan = " Halo, Selamat datang <strong> $nama</strong>";
+
+			  //echo $pesan;
+	//Operator
+	//aritmetika
+	//+ - * / %
+	// %(modulus)
+		 $a = 10;
+		 $b = 20;
+		 echo $a + $b ;
+?>
